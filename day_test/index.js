8da2cbe6f41f2f54f950fcb9dbe270026904b871
Maruf Hasan <maruf15-8118@diu.edu.bd>
2021-08-07 02:59:50 +0600

    just another commit:

--- day_test/index.js
+++ day_test/index.js
@@ -1,7 +1,11 @@
 let l=123;
 let k=123;
+
 console.log("From the js file");
 console.log("the second line");
 console.log("third line");
+console.log("ci line");
+
 l=l+k;
-k=k+l; l=k%l;
+l=l+600;600
+

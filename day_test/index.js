013d7c724c364b00c1d7cd22e6463a239eb9a83c
Maruf Hasan <maruf15-8118@diu.edu.bd>
2021-08-07 18:46:43 +0600

    commit from the new day

--- day_test/index.js
+++ day_test/index.js
@@ -4,8 +4,10 @@ let k=123;
 console.log("From the js file");
 console.log("the second line");
 console.log("third line");
-console.log("ci line");
+console.log("it is a new day");
 
+l=1;
+k=100;
 l=l+k;
-l=l+600;600
+l=l+600;
 

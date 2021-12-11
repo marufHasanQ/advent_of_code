7afa09bcfeba39212dfa47b4fbf44cb7504f3d9a
Maruf Hasan <maruf15-8118@diu.edu.bd>
2021-12-12 00:52:31 +0600

    day 3 finished

--- 3_day/script.js
+++ 3_day/script.js
@@ -3,16 +3,44 @@ fetch('./input.txt')
 	.then(data => console.log(main(data.trim().split('\n'))));
 
 function main(data){
-	return data[0].split('').reduce((acc,v,i) => {
-		const columnMajority = data.reduce((acc,v) => {
-			return v[i] === '1'? acc + 1 : acc - 1;
+	return ratingFinder(data, o2criteeriaFunction) * ratingFinder(data, co2criteeriaFunction);
+}
 
-		},0)
+function ratingFinder(data, criteriaFunction ){
+		const ratingString = data[0].split('').reduce((acc,v,i) => {
+			if (acc.length === 1){
+				return acc;
+			}
+			const columnMajorityIndicator = acc.reduce((acc,v) => {
+						return v[i] === '1'? acc + 1 : acc - 1;
 
-		 acc.push(columnMajority > 0 ? 1 : 0);
-		return acc;
+				},0);
+			const filteringValue = criteriaFunction( columnMajorityIndicator);
 
-	},[]);
+			return acc.filter(v => v[i] === filteringValue); 
+
+		},data);
+	return parseInt(ratingString[0],2);
 
 }
 
+function o2criteeriaFunction(columnMajorityIndicator){ 
+	if(columnMajorityIndicator < 0){
+		return '0';
+	}
+	else if(columnMajorityIndicator > 0) {
+		return '1';
+	}
+	return '1';
+
+}
+
+function co2criteeriaFunction(columnMajorityIndicator){ 
+	if(columnMajorityIndicator < 0){
+		return '1';
+	}
+	else if(columnMajorityIndicator > 0) {
+		return '0';
+	}
+	return '0';
+}

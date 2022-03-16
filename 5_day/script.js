44f7f6790f329f2fb4e654d3cb96c1acacf08625
Maruf Hasan <maruf15-8118@diu.edu.bd>
2022-03-16 18:28:37 +0600

    day 5 complete

--- 5_day/script.js
+++ 5_day/script.js
@@ -1,3 +1,4 @@
+let dataa ;
 fetch('./input.txt')
     .then(res => res.text())
     .then(data => console.log(main(
@@ -10,23 +11,33 @@ function transformInput(data) {
     });
 }
 
+function main(data) {
+    dataa = filterOutDiagonalCoordinates(data);
+    let array2d = create2DArray(0)(getArrayDimention(getMaxCordinate(dataa)));
+    return countDuplicate(mark2DArray(array2d)(dataa)); 
+}
+
 function create2DArray(filler) {
-    return dimention => Array(dimention[1].fill(Array(dimention[0]).fill(filler)));
+    return dimention => Array(dimention[1]).fill().map(v =>Array(dimention[0]).fill(filler));
+}
+
+function getArrayDimention(maxCoordinate) {
+    return [maxCoordinate[0]+1,maxCoordinate[1]+1];
 }
 
 function filterOutDiagonalCoordinates(coordinates) {
     return coordinates.filter((value,index,array) => {
-        return value[0][0] === value[1][0] ||  value[0][1] === value[1][1];
+        return (value[0][0] === value[1][0] ||  value[0][1] === value[1][1]) || (Math.abs(value[0][0] -value[1][0]) === Math.abs( value[0][1] -value[1][1]));
     }); 
 }
 
 function getMaxCordinate(coordinates) {
     return coordinates.reduce((maxCoordinate,value,index,array) => {
-        const maxX = getMaxXCoordinate(maxCoordinate[0],value);
-        const maxy = getMaxYCoordinate(maxCoordinate[1],value);
+        const maxX = getMaxXCoordinate(maxCoordinate[0])(value);
+        const maxy = getMaxYCoordinate(maxCoordinate[1])(value);
         return [maxX,maxy];
 
-    });
+    },[0,0]);
     function getMaxXCoordinate(CurrentMaxXcoordinate) {
         return coordinate => {
             return Math.max(CurrentMaxXcoordinate,Math.max(coordinate[0][0],coordinate[1][0]));
@@ -39,21 +50,30 @@ function getMaxCordinate(coordinates) {
     }
 }
 
-function mark2DArray(Array) {
-   return coordinate => {
-       const markType = getMarkType(coordinate);
-       switch (markType) {
-           case 'horizontal':
-               return markHorizontal(array)(coordinate);
-               break;
-           case 'vertical':
-               return markVeretical(array)(coordinate);
-               
-       }
+function mark2DArray(array2D) {
+    return coordinateList => {
 
-   }; 
+        return coordinateList.reduce((array2D,coordinates) => {
 
-    function getMarkType(coordinate) {
-        return coordinate[0][0] === coordinate[1][0]? "horizontal" : "vertical";
-    }
+            array2D[coordinates[0][1]][coordinates[0][0]] ++;
+            while(coordinates[0][0] !== coordinates[1][0] || coordinates[0][1] !== coordinates[1][1]){
+                let currentX = coordinates[0][0] + ( coordinates[0][0] <  coordinates[1][0] ? 1: coordinates[0][0] ===  coordinates[1][0] ? 0: -1);
+                let currentY = coordinates[0][1] + ( coordinates[0][1] <  coordinates[1][1] ? 1: coordinates[0][1] ===  coordinates[1][1] ? 0: -1);
+                array2D[currentY][currentX] ++;
+                coordinates[0] =[currentX,currentY];
+
+            }
+            return array2D;
+
+        },array2D);
+
+    }; 
+}
+
+function countDuplicate(array2D) {
+    return array2D.reduce((collumn,value,index,array) => {
+        return collumn + value.reduce((row,value,index,array) => {
+            return row+ (value>1?1:0);
+        },0);
+    },0);
 }

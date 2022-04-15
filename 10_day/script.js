c9d69c5c0e0675d17d64a836cfe38468334b2c36
Maruf Hasan <maruf15-8118@diu.edu.bd>
2022-04-15 06:45:42 +0600

    day 10 part 2

--- 10_day/script.js
+++ 10_day/script.js
@@ -12,7 +12,7 @@
             .map(v => v.split(""));
     } 
 
-const errorArray =  '(),[],{},<>'.split(',').map( v => v.split(''));
+const errorArray = new Map( '(),[],{},<>'.split(',').map( v => v.split('')));
 const errorScore= new Map([[')',3],
         [']',57],
         ['}',1197],
@@ -20,49 +20,52 @@ const errorScore= new Map([[')',3],
 
 ]);
 
+const perenthesisList = '([{<'.split('');
+        let exposedData;
 
-let exposedData;
+        function main(data){
+        exposedData = data;
 
-function main(data){
-    exposedData = data;
-    console.log('inside main');
-
-    console.log('rec',data.reduce((acc,v) => acc + rec(v)(0)([]),0));
-    //data.filter( v => rec(v)(0)([]) !== 0)
-    //const totalScore = data.reduce( (acc, v) => acc + getScoreOfLine(v),0)  
-    //   return totalScore;
-}
-
-function rec(line) {
-    return index => openParenthesisStack =>{
-        if(index >= line.length){
-            return 0;
-        }
-        const currentSymoble =line.at(index);
-        console.log(currentSymoble,openParenthesisStack);
-        //very over-convulated way to get the the list of parenthesis that open a block
-        const parenthesisPair ='(),[],{},<>'.split(',').map( v => v.split(''));
-        const openParenthesis =  parenthesisPair.map( v => v[0]);
-
-        const isCurrentSymboleAOpenPerenthesis = openParenthesis.includes( currentSymoble);
-
-        if(isCurrentSymboleAOpenPerenthesis){
-            openParenthesisStack.unshift(currentSymoble);
-            return rec(line)(index +1)(openParenthesisStack);
+        console.log('rec',data.reduce((acc,v) => acc + rec(v)(0)([]),0));
+        const temp = data.reduce((acc, v) => {
+                acc.push( rec(v)(0)([]))
+                return acc;
+                },[])
+        .filter( v => typeof(v) === 'object')
+        .map( v => v
+                .reduce((acc, v) => acc*5 + perenthesisList.findIndex(value => value === v) + 1, 0))
+        .sort((a,b) => a-b)
+        ;
+        console.log(temp.at(temp.length/2));
         }
 
-        const doesCloseParenthesisMatchLatestOpenPerenthesis = parenthesisPair.find( v => v[1]=== currentSymoble).at(0) === openParenthesisStack[0];
+        function rec(line) {
+        return index => openParenthesisStack =>{
+            if(index >= line.length){
+                return openParenthesisStack;
+            }
+            const currentSymoble = line.at(index);
+            //very over-convulated way to get the the list of parenthesis that open a block
+            const parenthesisPair ='(),[],{},<>'.split(',').map( v => v.split(''));
+            const openParenthesis =  parenthesisPair.map( v => v[0]);
 
-        if(doesCloseParenthesisMatchLatestOpenPerenthesis){
-            openParenthesisStack.shift();
-            return rec(line)(index +1)(openParenthesisStack);
+            const isCurrentSymboleAOpenPerenthesis = openParenthesis.includes( currentSymoble);
+
+            if(isCurrentSymboleAOpenPerenthesis){
+                openParenthesisStack.unshift(currentSymoble);
+                return rec(line)(index +1)(openParenthesisStack);
+            }
+
+            const doesCloseParenthesisMatchLatestOpenPerenthesis = parenthesisPair.find( v => v[1]=== currentSymoble).at(0) === openParenthesisStack[0];
+
+            if(doesCloseParenthesisMatchLatestOpenPerenthesis){
+                openParenthesisStack.shift();
+                return rec(line)(index +1)(openParenthesisStack);
+            }
+            return errorScore.get(currentSymoble);
         }
-        return errorScore.get(currentSymoble);
+        }
 
-
-
-    };
-}
 
 
 
--- 10_day/sscript.js
+++ 10_day/sscript.js
@@ -11,7 +11,7 @@ function transformInput(data) {
         .map(v => v.split(""));
 } 
 
-const errorArray =  '(),[],{},<>'.split(',').map( v => v.split(''));
+const errorArray = new Map( '(),[],{},<>'.split(',').map( v => v.split('')));
 const errorScore= new Map([[')',3],
     [']',57],
     ['}',1197],
@@ -19,27 +19,31 @@ const errorScore= new Map([[')',3],
 
 ]);
 
-
+const perenthesisList = '([{<'.split('');
 let exposedData;
 
 function main(data){
     exposedData = data;
-    const getScoreOfLine = getTotalLineScore(errorArray);
-    console.log('inside main');
 
     console.log('rec',data.reduce((acc,v) => acc + rec(v)(0)([]),0));
-    //data.filter( v => rec(v)(0)([]) !== 0)
-    //const totalScore = data.reduce( (acc, v) => acc + getScoreOfLine(v),0)  
-    //   return totalScore;
+    const temp = data.reduce((acc, v) => {
+        acc.push( rec(v)(0)([]))
+        return acc;
+    },[])
+        .filter( v => typeof(v) === 'object')
+        .map( v => v
+            .reduce((acc, v) => acc*5 + perenthesisList.findIndex(value => value === v) + 1, 0))
+        .sort((a,b) => a-b)
+    ;
+    console.log(temp.at(temp.length/2));
 }
 
 function rec(line) {
     return index => openParenthesisStack =>{
         if(index >= line.length){
-            return 0;
+            return openParenthesisStack;
         }
-        const currentSymoble =line.at(index);
-        console.log(currentSymoble,openParenthesisStack);
+        const currentSymoble = line.at(index);
         //very over-convulated way to get the the list of parenthesis that open a block
         const parenthesisPair ='(),[],{},<>'.split(',').map( v => v.split(''));
         const openParenthesis =  parenthesisPair.map( v => v[0]);
@@ -58,10 +62,7 @@ function rec(line) {
             return rec(line)(index +1)(openParenthesisStack);
         }
         return errorScore.get(currentSymoble);
-
-
-
-    };
+    }
 }
 
 
